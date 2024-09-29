3326d8617d545fc6123480089d33262414092845
happyuniv <happyunivee>
2024-09-29 18:03:23 +0900

    refactor. configure axios common config & refactor configs

--- src/lib/axios/AxiosModule.ts
+++ src/lib/axios/AxiosModule.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosRequestConfig } from 'axios'
+import { AxiosRequestConfig } from 'axios'
 import {
   WritePostRequestDto,
   UserListRequestInfo,
@@ -15,39 +15,6 @@ import {
 import { getAccessToken, getRefreshToken } from '@/lib/utils'
 
 const BASE_URL = import.meta.env.VITE_BASE_URL
-
-export type ConfigType = {
-  method: string
-  url: string
-  params?: { [key: string]: any }
-  data?: { [key: string]: any }
-  headers?: any
-}
-
-export type AwaitApiType<T> = {
-  success: boolean
-  message: string
-  data: T
-  total?: number
-}
-
-export type HeadersType = {
-  [key: string]: any
-}
-
-export type AwaitApiResponseType<T> = {
-  success: boolean
-  result: AwaitApiType<T> | null
-  error: any
-  headers?: HeadersType
-}
-
-export const authInstance = axios.create({
-  withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-})
 
 const publicConfig = (_config: AxiosRequestConfig) => {
   const config = {
@@ -79,79 +46,37 @@ const userConfig = (_config: AxiosRequestConfig) => {
   return config
 }
 
-const awaitApi = async (
-  _config: ConfigType
-): Promise<AwaitApiResponseType<any>> => {
-  try {
-    const response = await authInstance({
-      baseURL: BASE_URL,
-      ..._config,
-    })
-    return {
-      success: true,
-      result: response.data,
-      error: false,
-      headers: response.headers,
-    }
-  } catch (e) {
-    return {
-      success: false,
-      result: null,
-      error: e,
-    }
-  }
-}
-
 // ==============================
 /* 로그인/로그아웃/회원가입 기능 */
 // ==============================
-export const createSignupConfig = (
-  requestBody: SignupInfo
-): AxiosRequestConfig => {
-  const config: AxiosRequestConfig = {
-    baseURL: BASE_URL,
+export const createSignupConfig = (data: SignupInfo) => {
+  const config = publicConfig({
     url: '/sign',
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    data: requestBody,
-  }
+    data,
+  })
 
   return config
 }
 
-export const createSigninConfig = (
-  requestBody: SigninInfo
-): AxiosRequestConfig => {
-  const config: AxiosRequestConfig = {
-    baseURL: BASE_URL,
+export const createSigninConfig = (data: SigninInfo): AxiosRequestConfig => {
+  const config = publicConfig({
     url: '/login',
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    data: requestBody,
-    withCredentials: true,
-  }
+    data,
+  })
 
   return config
 }
 
-export const createLogOutConfig = (): AxiosRequestConfig => {
-  const refresh = getRefreshToken()
-  const access = getAccessToken()
-  const config: AxiosRequestConfig = {
-    baseURL: BASE_URL,
+export const createLogOutConfig = () => {
+  const config = userConfig({
     url: '/service-logout',
     method: 'POST',
     headers: {
-      Access_Token: access,
-      'Refresh-Token': refresh,
       'Content-Type': 'application/json',
     },
-    withCredentials: true,
-  }
+  })
 
   return config
 }
@@ -181,102 +106,52 @@ export const createMainlistConfig = (
 // ==============================
 /* 게시판 API */
 // ==============================
-export const createWriteConfig = (
-  requestBody: WritePostRequestDto
-): AxiosRequestConfig => {
-  const refresh = getRefreshToken()
-  const access = getAccessToken()
-  const config: AxiosRequestConfig = {
-    baseURL: BASE_URL,
+export const createWriteConfig = (data: WritePostRequestDto) => {
+  const config = userConfig({
     url: '/board/member',
     method: 'POST',
-    headers: {
-      Access_Token: access,
-      'Refresh-Token': refresh,
-      'Content-Type': 'application/json',
-    },
-    data: requestBody,
-  }
+    data,
+  })
 
   return config
 }
 
-export const createReadPostConfig = (boardId: string): AxiosRequestConfig => {
-  const config: AxiosRequestConfig = {
-    baseURL: BASE_URL,
+export const createReadPostConfig = (boardId: string) => {
+  const config = publicConfig({
     url: `/board/${boardId}`,
     method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    withCredentials: true,
-  }
+  })
 
   return config
 }
 
-export const createModifyPostConfig = (
-  requestBody: ModifyPostInfo
-): AxiosRequestConfig => {
-  const access = getAccessToken()
-  const refresh = getRefreshToken()
-
-  const config: AxiosRequestConfig = {
-    baseURL: BASE_URL,
+export const createModifyPostConfig = (data: ModifyPostInfo) => {
+  const config = userConfig({
     url: `/board/member`,
     method: 'PATCH',
-    headers: {
-      Access_Token: access,
-      Refresh_Token: refresh,
-      'Content-Type': 'application/json',
-    },
-    data: requestBody,
-    withCredentials: true,
-  }
+    data,
+  })
 
   return config
 }
 
-export const createModifyRecruitConfig = (
-  requestBody: ModifyRecruitInfo
-): AxiosRequestConfig => {
-  const access = getAccessToken()
-  const refresh = getRefreshToken()
-
-  const config: AxiosRequestConfig = {
-    baseURL: BASE_URL,
-    url: `/board/member/recurit/${requestBody.boardId}`,
+export const createModifyRecruitConfig = (data: ModifyRecruitInfo) => {
+  const config = userConfig({
+    url: `/board/member/recurit/${data.boardId}`,
     method: 'PATCH',
-    headers: {
-      Access_Token: access,
-      Refresh_Token: refresh,
-      'Content-Type': 'application/json',
-    },
-    data: requestBody.recruit,
-    withCredentials: true,
-  }
+    data: data.recruit,
+  })
   return config
 }
 
-export const createRemovePostConfig = (
-  requestBody: DeletePostInfo
-): AxiosRequestConfig => {
+export const createRemovePostConfig = (requestBody: DeletePostInfo) => {
   const { boardId } = requestBody
-  const access = getAccessToken()
-  const refresh = getRefreshToken()
 
-  const config: AxiosRequestConfig = {
-    baseURL: BASE_URL,
+  const config = userConfig({
     url: `/board/member/${boardId}`,
     method: 'DELETE',
-    headers: {
-      Access_Token: access,
-      Refresh_Token: refresh,
-      'Content-Type': 'application/json',
-    },
     params: requestBody,
-    withCredentials: true,
-  }
+  })
 
   return config
 }
@@ -284,95 +159,51 @@ export const createRemovePostConfig = (
 // ==============================
 /* 마이페이지 기능 */
 // ==============================
-export const createUserPostConfig = (
-  requestBody: UserListRequestInfo
-): AxiosRequestConfig => {
-  const { recruit, category, order } = requestBody
-  const access = getAccessToken()
-  const refresh = getRefreshToken()
-  const config: AxiosRequestConfig = {
-    baseURL: BASE_URL,
+export const createUserPostConfig = (data: UserListRequestInfo) => {
+  const { recruit, category, order } = data
+  const config = userConfig({
     url: `/user/lists`,
     method: 'GET',
-    headers: {
-      Access_Token: access,
-      Refresh_Token: refresh,
-      'Content-Type': 'application/json',
-    },
     params: {
       recruit,
       category,
       order,
     },
-    withCredentials: true,
-  }
+  })
 
   return config
 }
 
-export const createGetUserInfoConfig = (): AxiosRequestConfig => {
-  const access = getAccessToken()
-  const refresh = getRefreshToken()
-
-  const config: AxiosRequestConfig = {
-    baseURL: BASE_URL,
+export const createGetUserInfoConfig = () => {
+  const config = userConfig({
     url: `/user/info`,
     method: 'GET',
-    headers: {
-      Access_Token: access,
-      Refresh_Token: refresh,
-      'Content-Type': 'application/json',
-    },
-    withCredentials: true,
-  }
+  })
 
   return config
 }
 
-export const createModifyUserInfoConfig = (
-  requestBody: ModifyUserInfo
-): AxiosRequestConfig => {
-  const access = getAccessToken()
-  const refresh = getRefreshToken()
-
-  const config: AxiosRequestConfig = {
-    baseURL: BASE_URL,
+export const createModifyUserInfoConfig = (data: ModifyUserInfo) => {
+  const config = userConfig({
     url: `/user/info`,
     method: 'PATCH',
-    headers: {
-      Access_Token: access,
-      Refresh_Token: refresh,
-      'Content-Type': 'application/json',
-    },
-    data: requestBody,
-    withCredentials: true,
-  }
+    data,
+  })
 
   return config
 }
 
-export const createMyLikePostConfig = (
-  requestBody: UserListRequestInfo
-): AxiosRequestConfig => {
-  const { recruit, category, order } = requestBody
-  const access = getAccessToken()
-  const refresh = getRefreshToken()
-  const config: AxiosRequestConfig = {
-    baseURL: BASE_URL,
+export const createMyLikePostConfig = (data: UserListRequestInfo) => {
+  const { recruit, category, order } = data
+  const config = userConfig({
     url: `/user/post-likes`,
     method: 'GET',
-    headers: {
-      Access_Token: access,
-      Refresh_Token: refresh,
-      'Content-Type': 'application/json',
-    },
     params: {
       recruit,
       category,
       order,
     },
-    withCredentials: true,
-  }
+  })
 
   return config
 }
@@ -380,128 +211,55 @@ export const createMyLikePostConfig = (
 // ==============================
 /* 댓글 기능 구현 */
 // ==============================
-export const createAddCommentConfig = (
-  requestBody: AddCommentRequestDto
-): AxiosRequestConfig => {
-  const access = getAccessToken()
-
-  const config: AxiosRequestConfig = {
-    baseURL: BASE_URL,
+export const createAddCommentConfig = (data: AddCommentRequestDto) => {
+  const config = userConfig({
     url: `/reply`,
     method: 'POST',
-    headers: {
-      Access_Token: access,
-      'Content-Type': 'application/json',
-    },
-    data: requestBody,
-  }
+    data,
+  })
 
   return config
 }
 
-export const createEditCommentConfig = (
-  requestBody: EditCommentRequestDto
-): AxiosRequestConfig => {
-  const access = getAccessToken()
-  const refresh = getRefreshToken()
-
-  const config: AxiosRequestConfig = {
-    baseURL: BASE_URL,
+export const createEditCommentConfig = (data: EditCommentRequestDto) => {
+  const config = userConfig({
     url: `/reply`,
     method: 'PATCH',
-    headers: {
-      Access_Token: access,
-      Refresh_Token: refresh,
-      'Content-Type': 'application/json',
-    },
-    data: requestBody,
-    withCredentials: true,
-  }
+    data,
+  })
 
   return config
 }
 
-export const createDeleteCommentConfig = (
-  requestBody: string
-): AxiosRequestConfig => {
-  const access = getAccessToken()
-  const refresh = getRefreshToken()
-
-  const config: AxiosRequestConfig = {
-    baseURL: BASE_URL,
+export const createDeleteCommentConfig = (requestBody: string) => {
+  const config = userConfig({
     url: `/reply/${requestBody}`,
     method: 'DELETE',
-    headers: {
-      Access_Token: access,
-      Refresh_Token: refresh,
-      'Content-Type': 'application/json',
-    },
     params: {
       rno: requestBody,
     },
-    withCredentials: true,
-  }
+  })
 
   return config
-}
-
-export const deleteComment = async (replyId: number) => {
-  const access = getAccessToken()
-  const refresh = getRefreshToken()
-
-  return awaitApi({
-    method: 'DELETE',
-    url: `/reply/${replyId}`,
-    headers: {
-      Access_Token: access,
-      Refresh_Token: refresh,
-      'Content-Type': 'application/json',
-    },
-    params: {
-      rno: replyId.toString(),
-    },
-  })
 }
 
 // ==============================
 /* 사용자 관심 글 */
 // ==============================
-export const createPostLikePostConfig = (
-  boardId: string
-): AxiosRequestConfig => {
-  const access = getAccessToken()
-  const refresh = getRefreshToken()
-  const config: AxiosRequestConfig = {
-    baseURL: BASE_URL,
+export const createPostLikePostConfig = (boardId: string) => {
+  const config = userConfig({
     url: `/post-like/${boardId}`,
     method: 'POST',
-    headers: {
-      Access_Token: access,
-      Refresh_Token: refresh,
-      'Content-Type': 'application/json',
-    },
-    withCredentials: true,
-  }
+  })
 
   return config
 }
 
-export const DeleteLikePostConfig = (
-  postLikeId: string
-): AxiosRequestConfig => {
-  const access = getAccessToken()
-  const refresh = getRefreshToken()
-  const config: AxiosRequestConfig = {
-    baseURL: BASE_URL,
+export const DeleteLikePostConfig = (postLikeId: string) => {
+  const config = userConfig({
     url: `/post-like/${postLikeId}`,
     method: 'DELETE',
-    headers: {
-      Access_Token: access,
-      Refresh_Token: refresh,
-      'Content-Type': 'application/json',
-    },
-    withCredentials: true,
-  }
+  })
 
   return config
 }
@@ -509,70 +267,37 @@ export const DeleteLikePostConfig = (
 // ==============================
 /* 관리자 기능 */
 // ==============================
-export const createGetAllUsersInfoConfig = (
-  username?: string
-): AxiosRequestConfig => {
-  const access = getAccessToken()
-  const refresh = getRefreshToken()
-  const config: AxiosRequestConfig = {
-    baseURL: BASE_URL,
+export const createGetAllUsersInfoConfig = (username?: string) => {
+  const config = userConfig({
     url: `/admin/user-all`,
     method: 'GET',
-    headers: {
-      Access_Token: access,
-      Refresh_Token: refresh,
-      'Content-Type': 'application/json',
-    },
     params: {
       username: username,
     },
-    withCredentials: true,
-  }
+  })
 
   return config
 }
 
-export const createGetAllUsersPostConfig = (
-  requestBody: UserListRequestInfo
-): AxiosRequestConfig => {
-  const { recruit, category, order } = requestBody
-  const access = getAccessToken()
-  const refresh = getRefreshToken()
-  const config: AxiosRequestConfig = {
-    baseURL: BASE_URL,
+export const createGetAllUsersPostConfig = (data: UserListRequestInfo) => {
+  const { recruit, category, order } = data
+  const config = userConfig({
     url: `/admin/dash-board`,
     method: 'GET',
-    headers: {
-      Access_Token: access,
-      Refresh_Token: refresh,
-      'Content-Type': 'application/json',
-    },
     params: {
       recruit,
       category,
       order,
     },
-    withCredentials: true,
-  }
+  })
   return config
 }
 
-export const createAdminRemovePostConfig = (
-  boardId: string
-): AxiosRequestConfig => {
-  const access = getAccessToken()
-  const refresh = getRefreshToken()
-  const config: AxiosRequestConfig = {
-    baseURL: BASE_URL,
+export const createAdminRemovePostConfig = (boardId: string) => {
+  const config = userConfig({
     url: `/admin/board/${boardId}?role=ROLE_ADMIN`,
     method: 'DELETE',
-    headers: {
-      Access_Token: access,
-      Refresh_Token: refresh,
-      'Content-Type': 'application/json',
-    },
-    withCredentials: true,
-  }
+  })
 
   return config
 }
